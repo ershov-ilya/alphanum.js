5c0e4c23129c0fbfdf458d4add87e129a1f8d262
ershov-ilya <ershov.ilya@gmail.com>
2015-09-07 11:06:07 +0300

    encode function

--- alphanum.js
+++ alphanum.js
@@ -12,7 +12,20 @@ var AlphaNum=(function(){
     var PUBLIC={};
 
     // Private methods
-    var config={};
+    var config={
+        alphabet:'0123456789abcdefghijkmnopqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ',
+        base:60
+    };
+
+    PUBLIC.init=function($alphabet){
+        if(typeof $alphabet == 'undefined') $alphabet='0123456789abcdefghijkmnopqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ';
+        config.alphabet=$alphabet;
+        config.base=$alphabet.length;
+    };
+
+    PUBLIC.test = function(){
+        console.log(config);
+    };
 
     PUBLIC.config=function(options){
         if(typeof options == 'undefined'){
@@ -28,8 +41,38 @@ var AlphaNum=(function(){
         }
         return false;
     };
-    PUBLIC.encode=function(input){};
-    PUBLIC.parse=function(input){};
+
+    PUBLIC.encode=function($input){
+        var $alphabet=config.alphabet;
+        var $base=config.base;
+        var $num=parseInt($input,10);
+        var $output='', $rest, $sym;
+        do {
+            $rest = $num % $base;
+            $sym=$alphabet[$rest];
+            $output+=$sym;
+            $num = Math.floor($num / $base);
+        }while($num>0);
+        $output=$output.split("").reverse().join("");
+        return $output;
+    };
+
+    /*
+    PUBLIC.parse=function($input){
+        $alphabet=AlphaNum::$alphabet;
+        $base=AlphaNum::$base;
+        $str=rtrim($input,"\n\r");
+        $res=0;
+        $len=strlen($str);
+        $last=$len-1;
+        for($i=0; $i<$len; $i++){
+            $pos=strpos($alphabet,$str[$i]);
+            $res+=$pos;
+            if($i!=$last) $res *= $base;
+        }
+        return $res;
+    };
+    */
 
     return PUBLIC;
 })();
